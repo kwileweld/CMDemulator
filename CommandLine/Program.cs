6194c8f0f28fd87f21dc47ed8f2d91fb8bf12e2f
Alexander <kwileweld@mail.ru>
2020-12-23 15:03:43 +0300

    Создание файла, его удаление , запись, чтение

--- CommandLine/Program.cs
+++ CommandLine/Program.cs
@@ -12,6 +12,11 @@ namespace Командная_Строка
             Console.WriteLine("/diskinfo - информация о диске", "\n");
             Console.WriteLine("/cls - очистить консоль", "\n");
             Console.WriteLine("/catalog - информация о файле", "\n");
+            Console.WriteLine("/create - создание файла","\n");
+            Console.WriteLine("/copyfile - создание файла", "\n");
+            Console.WriteLine("/delfile - удаление файла", "\n");
+            Console.WriteLine("/wrfile - запись данных в файл", "\n");
+            Console.WriteLine("/rdfile - чтение из файла", "\n");
         }
         static void cls()
         {
@@ -33,14 +38,12 @@ namespace Командная_Строка
             try
             {
                 Directory.Move(newPath, oldPath);
-
             }
             catch (Exception e)
             { 
                 Console.WriteLine(e.Message);
             
             }
-            
         }
         static void DiskInfo()
         {
@@ -50,14 +53,52 @@ namespace Командная_Строка
                 Console.WriteLine($"Название: {drive.Name}");
                 Console.WriteLine($"Свободного места на диске:{drive.AvailableFreeSpace}");
                 Console.WriteLine($"Тип: {drive.DriveFormat}");
-
+            }
+        }
+        static void CreateFile()
+        {
+            Console.WriteLine("Введите путь и название файла");
+            string file = Console.ReadLine();
+                File.Create(file); 
+        }
+        static void CopyFIle()
+        {
+            Console.WriteLine("Укажите путь к файлу и путь, в который нужно скопировать");
+            string sourse;
+            sourse = Console.ReadLine();
+            string destination;
+            destination = Console.ReadLine();
+            File.Copy(sourse, destination);
+        }
+        static void DeleteFile()
+        {
+            Console.WriteLine("Укажите путь к файлу который хотите удалить");
+            string path;
+            path = Console.ReadLine();
+            File.Delete(path);
+            
+        }
+        static void WriteFile()
+        {
+            Console.WriteLine("Введите текст");
+            string text = Console.ReadLine();
+            Console.WriteLine("Укажите путь к файлу, в который записать информацию");
+            string path = Console.ReadLine();
+            File.WriteAllText(path, text);
+        }
+        static void ReadFile()
+        {
+            Console.WriteLine("Введите путь к файлу, из которого нужно читать данные");
+            string path = Console.ReadLine();
+            string[] lines = File.ReadAllLines(path);
+            foreach (String line in lines)
+                {
+                Console.WriteLine(line);
             }
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Введите команду /info, чтобы узнать больше о доступных программах");
-
-
             string command;
             do
             {
@@ -79,12 +120,24 @@ namespace Командная_Строка
                     case "/goto":
                         AnotherDirectory();
                         break;
-
+                    case "/create":
+                        CreateFile();
+                        break;
+                    case "/copyfile":
+                        CopyFIle();
+                        break;
+                    case "/delfile":
+                        DeleteFile();
+                        break;
+                    case "/wrfile":
+                        WriteFile();
+                        break;
+                    case "/rdfile":
+                        ReadFile();
+                        break;
                 }
 
             } while (command != "/exit");
-
-
 
         }
     }
